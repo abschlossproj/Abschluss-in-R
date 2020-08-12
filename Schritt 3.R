280b586b010d32d4b7a646b70de5d3445056fa8f
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2020-08-12 19:26:06 +0200

    visualisations weitergebracht

--- Schritt 3.R
+++ Schritt 3.R
@@ -1,7 +1,10 @@
+#install.packages('tidyverse')
+#install.packages('ggplot2')
+
 library(ggplot2)
 library(tidyverse)
 
-WorkingDirectory = readline(prompt="Enter the working directory or press [Enter] if your on matthias's machine: ")
+WorkingDirectory = '.'
 
 paste('Working directory:', WorkingDirectory)
 
@@ -22,17 +25,15 @@ files = list.files(file.path(WorkingDirectory, 'documentation_tables'))
 docs = data.frame()
 for (x in files) {
   df = read_delim(file.path(WorkingDirectory,'documentation_tables' , x), delim=',') 
-  df = df %>% mutate(
-                    k = as.integer(str_sub(x, 22, 23)),
-                    m = as.integer(str_sub(x, 26, 27)),
-                    p = as.integer(str_sub(x, 30, 31))
-                    )
+  df = df %>% mutate(k = as.integer(str_sub(x, 22, 23)),
+                     m = as.integer(str_sub(x, 25, 26)),
+                     p = as.integer(str_sub(x, 28, 29)))
   docs = bind_rows(docs, df)  
 }
 
 t = 105
 
-docm05 = docs[docs['m']==5,]
+docm05 = docs[docs['m'] == 5,]
 file = files[25] #arbitrarely chosen scenario with a nice development
 
 doc = read_delim(file.path(WorkingDirectory,'documentation_tables', file), delim=',')
@@ -41,23 +42,24 @@ doc = read_delim(file.path(WorkingDirectory,'documentation_tables', file), delim
 
 docm05_cp = docm05
 docm05_cp = mutate(docm05_cp, 
-                   k = toString('k'), 
-                   p = toString('p'))
+                   k = toString(k), 
+                   p = toString(p))
 
-doc_interest = docm05_cp[docm05_cp['days'] < 10]
+doc_interest = docm05_cp[docm05_cp['day'] < 10]
 
 # tägliche Neuinfektionen, vs Veränderung Infizierter vs Tote
-D_chng = c() 
-D_nw = c() 
-R_nw = c() 
-T_nw = c()
+len = nrow(doc)
 
-for (i in 2:length(doc)) {
-  
-  D_chng = c(D_chng, doc[i,]['D'] - doc[i-1,]['D'])
-  D_nw = c(D_chng, doc[i-1,]['H'] - doc[i,]['H'])
-  R_nw = c(D_chng, doc[i,]['R'] - doc[i-1,]['R'])
-  T_nw = c(D_chng, doc[i,]['T'] - doc[i-1,]['T'])
+D_chng = integer(len)
+D_nw = integer(len) 
+R_nw = integer(len)
+T_nw = integer(len)
+
+for (i in 2:nrow(doc)) {
+  D_chng[i] = doc[i,]['D'] - doc[i-1,]['D']
+  D_nw[i] = doc[i-1,]['H'] - doc[i,]['H']
+  R_nw[i] = doc[i,]['R'] - doc[i-1,]['R']
+  T_nw[i] = doc[i,]['T'] - doc[i-1,]['T']
 }
 
 doc_change = tibble(H = doc$H,
@@ -66,24 +68,23 @@ doc_change = tibble(H = doc$H,
                     D_Change = D_chng,
                     D_New = D_nw,
                     R_New = R_nw,
-                    days = doc$days,
+                    days = doc$day,
                     T = doc$T,
-                    T_new = T_nw * -1,
+                    #T_new = T_nw * -1,
                     T_new_pos = T_nw,
                     T_new_log = sapply(T_nw, log10))
 
 #cumsums -> makes overlapping possible, doc_c['T'] (not doc_c.T) actually is always 5000
-doc_c = doc.copy()
-doc_c['D'] = doc_c['D']+doc_c['H']
-doc_c['R'] = doc_c['R']+doc_c['D']
-doc_c['T'] = doc_c['T']+doc_c['R']
+doc_c = doc
+doc_c['D'] = doc_c['D'] + doc_c['H']
+doc_c['R'] = doc_c['R'] + doc_c['D']
+doc_c['T'] = doc_c['T'] +  doc_c['R']
 
 # logaithmierte D und T
-doc_logT = doc['T'].apply(lambda x: math.log(log10(x), 10)) #sehr langsam :/
-doc_logD = doc['D'].apply(lambda x: math.log(log10(x), 10)) # -> np.log(x) (geht auch mit df/ser?!)
-doc_logIso = doc.Iso.apply(lambda x: math.log(log10(x),10))
-doc_logDT = pd.DataFrame((doc['days'], doc_logT, doc_logD, doc_logIso)).T
-
+doc_logT = sapply(doc['T'], log10)
+doc_logD = sapply(doc['D'], log10)
+doc_logIso = sapply(doc['Iso'], log10)
+doc_logDT = tibble(doc['day'], doc_logT, doc_logD, doc_logIso)
 
 #################
 # 2 Plotting    #
@@ -91,37 +92,40 @@ doc_logDT = pd.DataFrame((doc['days'], doc_logT, doc_logD, doc_logIso)).T
 
 
 # units for saving
-w=15; h=10; u='cm'; dpi=300
+w=15
+h=10
+u='cm'
+dpi=300
 
 # Mimicking the ZIB Plots on Instagramm
-ggsave(filename = paste0(graphics , 'ZIB-like.png'), width=w, height=w, units=u, dpi=dpi, plot=
-         ggplot(doc_change, aes(x= days, y=D_new)) +
-          geom_col(fill='white', width=.8) + geom_smooth(color='yellow', size=1.5)+
-          labs(title='XYZ Virus in Scenario ABC:\n Tägliche Neuinfektionen', y='')+
-          theme_classic()+
-          theme( rect=element_rect(color='#0072B2', size=0, fill='#0072B2'),
+ggsave(filename = paste0(graphics , '/ZIB-like.png'), width = w, height = w, units= u , dpi = dpi, 
+       plot = ggplot(doc_change, aes(x = days, y = D_New)) +
+          geom_col(fill = 'white', width = .8) + geom_smooth(color = 'yellow', size = 1.5) +
+          labs(title = 'XYZ Virus in Scenario ABC:\n Tägliche Neuinfektionen', y = '') +
+          theme_classic() +
+          theme(rect = element_rect(color = '#0072B2', size = 0, fill = '#0072B2'),
                 axis.text = element_text(colour = 'white'), axis.title = element_text(colour = 'white'), 
-                axis.line = element_line(color='white'), plot.title = element_text(colour = 'white'))+
-          xlim(20,55) + ylim(0,180))#value in R as c()?
+                axis.line = element_line(color='white'), plot.title = element_text(colour = 'white')) +
+          xlim(20,55) + ylim(0,180)) #value in R as c()?
 
 
 g = ggplot(docm05, aes(x='days'))
 
 # facet über p, k, m fix 5
-ggsave(filename = paste0(graphics , 'pkm_facet.png'), width=w, height=h, units=u, dpi=dpi, plot=
+ggsave(filename = paste0(graphics , '/pkm_facet.png'), width=w, height=h, units=u, dpi=dpi, plot=
          g + geom_line(aes(y=T))  + labs(title='Herding immunity works, \nHygiene and Distancing is better', y='virus fatalities')+
           facet_grid(k~p, labeller='label_both'))
 # interesting: for (20,50) there's less deads in total than vor (10,25)
 
 
 # line-graphs, die die totalen Infizierten über verschiedenen p,k vergleichen
-ggsave(filename = paste0(graphics , 'line_pk.png'), width=w, height=1.5*h, units=u, dpi=dpi, plot=
-         g + geom_line(aes(y=D, color=k, fill=k)) + facet_wrap(p~k), labeller='label_both')+
-          labs(y='infected persons', title='Only the strictest measures flatten the curve') + scale_color_gradient(guide=False) + theme_void())
+ggsave(filename = paste0(graphics , '/line_pk.png'), width=w, height=1.5*h, units=u, dpi=dpi, plot=
+         g + geom_line(aes(y=D, color=k, fill=k)) + facet_wrap(p~k))+
+          labs(y='infected persons', title='Only the strictest measures flatten the curve') + scale_color_gradient(guide=False) + theme_void()
 
 g_int = ggplot(doc_interest, aes(x=days,y=D))
 
-ggsave(filename = paste0(graphics , 'line_pk-log.png'), width=w, height=h, units=u, dpi=dpi, plot=
+ggsave(filename = paste0(graphics , '/line_pk-log.png'), width=w, height=h, units=u, dpi=dpi, plot=
          g_int + geom_line(aes(colour=k,linetype=p)) + scale_y_log10()+
           labs(y='Infected persons', title='Cases for some scenarios from \'day zero\'') + theme_linedraw())
 
@@ -134,11 +138,11 @@ g_col = ggplot(doc, aes(x= days)) +
    geom_line(aes(y=R),color='#4101a2',position='stack',alpha=0.4) +
    geom_line(aes(y=T),color='#b21281',position='stack',alpha=0.4) 
 
-ggsave(filename = paste0(graphics , 'time_line-col-log.png'), width=w, height=h, units=u, dpi=dpi, plot=
+ggsave(filename = paste0(graphics , '/time_line-col-log.png'), width=w, height=h, units=u, dpi=dpi, plot=
          g_col + scale_y_log10() + theme_bw() )
 
 # "gestacktes" Säulendiagramm, sodass gut die Verhältnisse sichtbar sind
-ggsave(filename = paste0(graphics , 'time_line-colstacked.png'), width=w, height=h, units=u, dpi=dpi, plot=
+ggsave(filename = paste0(graphics , '/time_line-colstacked.png'), width=w, height=h, units=u, dpi=dpi, plot=
          ggplot(doc_c, aes(x=days)) +
           geom_col(aes(y=T),fill='red',position='stack') +
           geom_col(aes(y=R),fill='green',position='stack') +
@@ -150,14 +154,14 @@ ggsave(filename = paste0(graphics , 'time_line-colstacked.png'), width=w, height
           annotate(geom='text', x=14, y=1900,label='Healthy')+
           annotate(geom='text', x=75, y=4850,label='Dead'))
 
-ggsave(filename = paste0(graphics , 'time_line_linesimple.png'), width=w, height=h, units=u, dpi=dpi, plot=            
+ggsave(filename = paste0(graphics , '/time_line_linesimple.png'), width=w, height=h, units=u, dpi=dpi, plot=            
          ggplot(doc_logDT, aes(x=days))+
           geom_line(aes(y=D),color='#0072B2') +
           geom_line(aes(y=Iso),color='#56B4E9') +
           geom_line(aes(y=T),color='#D55E00') +
           labs(title='Isolating helps keeping fatalities low', y='cases and deaths, log10'))
 
-ggsave(filename = paste0(graphics , 'changes.png'), width=w, height=h, units=u, dpi=dpi, plot=
+ggsave(filename = paste0(graphics , '/changes.png'), width=w, height=h, units=u, dpi=dpi, plot=
          ggplot(doc_change, aes(x=days))  +
           geom_col(aes(y=D_new), position='dodge',fill= 'orange', width=0.8) +
           geom_col(aes(y=R_new), position='dodge',fill='blue', width=0.8,alpha=.6) +
